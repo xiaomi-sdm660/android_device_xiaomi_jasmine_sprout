56dbe6535893a8ac0b3a17420438eaa8b3009beb
pqhaz <usaguy1342@gmail.com>
2019-05-17 13:32:24 +0000

    sdm660: AOSiP-ify
    Signed-off-by: pqhaz <usaguy1342@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -24,18 +24,15 @@
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
-# Inherit from Havoc custom product configuration
-$(call inherit-product, vendor/havoc/config/common.mk)
+# Inherit from AOSiP custom product configuration
+$(call inherit-product, vendor/aosip/config/common.mk)
 
 TARGET_VENDOR_PRODUCT_NAME := jasmine_sprout
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.havoc.maintainer=rcstar6696
 
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PLATFORM := SDM660
-PRODUCT_NAME := havoc_jasmine_sprout
+PRODUCT_NAME := aosip_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := MI A2
