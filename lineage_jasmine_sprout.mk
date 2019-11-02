3842879310ae8a9dd34709438b7fc627244d2d0e
Max Weffers <rcstar6696@gmail.com>
2019-11-02 16:48:17 +0100

    jasmine: Move Build fingerprint to common

--- lineage_jasmine_sprout.mk
+++ lineage_jasmine_sprout.mk
@@ -36,8 +36,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
 
-BUILD_FINGERPRINT := xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.9.0.PDIMIXM:user/release-keys
-
 # Device identifier
 PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
