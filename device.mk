e58983405bad054fbd8ecc1fa9995c304d592333
Aryan Gupta <guptaaryan189@gmail.com>
2020-01-29 08:13:53 +0000

    jasmine: add device specific cryptfs_hw
    since it's nuked in common tree
    
    Change-Id: I284b9d3335abff4be56ad28c66b5dd67bf126550

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 # Device properties
 $(call inherit-product, $(DEVICE_PATH)/device_prop.mk)
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Media
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
