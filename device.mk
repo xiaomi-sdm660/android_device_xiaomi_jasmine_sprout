bdc8ba91d2c63aa3e83ec54e4d4343639b5cbe6e
Stylogey <stylogey@gmail.com>
2020-05-03 18:15:23 +0200

    jasmine: move A/B stuff to common

--- device.mk
+++ device.mk
@@ -28,20 +28,7 @@ $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 DEVICE_PATH := device/xiaomi/jasmine_sprout
 
 # A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    vendor
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script
+ENABLE_AB := true
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -51,18 +38,6 @@ PRODUCT_COPY_FILES += \
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
-
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    bootctrl.sdm660 \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.sdm660.recovery
-
-# Boot control debug
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
 
 # Consumerir
 PRODUCT_PACKAGES += \
@@ -107,18 +82,6 @@ PRODUCT_COPY_FILES += \
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
-
-# Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_HOST_PACKAGES += \
-        brillo_update_payload
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
 
 # Vibrator
 PRODUCT_PACKAGES += \
