9061ec86b92cee95aa6c9a8a5c5b328654bdb219
Max Weffers <rcstar6696@gmail.com>
2020-05-06 12:02:00 +0200

    jasmine: Set Bootanimation Res

--- bliss_jasmine_sprout.mk
+++ bliss_jasmine_sprout.mk
@@ -30,6 +30,9 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
