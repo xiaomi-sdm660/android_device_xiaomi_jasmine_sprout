c64e634ae836a3d72b6f186516b5624c5e2b3ef3
dianlujitao <dianlujitao@lineageos.org>
2020-06-23 12:04:45 +0800

    jasmine: Remove keymap of headset buttons
    Change-Id: Ib64eb55a74e9ddfe53a0c0f8310cc0360c178d56

--- device.mk
+++ device.mk
@@ -64,10 +64,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
