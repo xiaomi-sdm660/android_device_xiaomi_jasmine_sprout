0486933cab1dc19dfe102f00aadc30455ec16d14
OdSazib <odsazib@gmail.com>
2020-07-30 23:35:14 +0600

    Jasmine_Sprout: Update maintainer prop

--- device_prop.mk
+++ device_prop.mk
@@ -94,3 +94,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.iwlan.enable=true \
     persist.vendor.radio.data_con_rprt=1
+    
+# Maintainer prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    org.evolution.build_maintainer=OdSazib \
+    org.evolution.build_support_url=https://t.me/EvolutionXJasmine_Sprout \
+    org.evolution.build_donate_url=https://www.paypal.me/belhodu
