b40d11df5b3235a95423423edb48ee2d8724f31d
OdSazib <odsazib@gmail.com>
2021-03-10 16:03:14 +0600

    jasmine_sprout: Decommonize build.fingerprint * wayne oreo fp always pass cts for   all supported device in common tree

--- awaken_jasmine_sprout.mk
+++ awaken_jasmine_sprout.mk
@@ -40,9 +40,12 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Build Fingerprint
+# Build Description
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
+
+# Build Fingerprint
+BUILD_FINGERPRINT := "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys"
 
 # Density
 TARGET_SCREEN_DENSITY := 400
